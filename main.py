0a6df8bc66b26dc58d47824bc2dca944cc4110e8
robotescu <80849623+robotescu@users.noreply.github.com>
2022-01-17 23:28:31 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,42 @@
+
+bila = game.create_sprite(randint(0, 4), 4)
+ciocan1 = game.create_sprite(2, 1)
+ciocan2 = game.create_sprite(2, 2)
+score = 0
+timp_pornire = input.running_time()
+
+def on_button_pressed_a():
+    ciocan1.change(LedSpriteProperty.X, -1)
+    ciocan2.change(LedSpriteProperty.X, -1)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    ciocan1.change(LedSpriteProperty.X, 1)
+    ciocan2.change(LedSpriteProperty.X, 1)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_button_pressed_ab():
+    global ciocan1, ciocan2
+    ciocan1.change(LedSpriteProperty.Y, 2)
+    ciocan2.change(LedSpriteProperty.Y, 2)
+    if ciocan2.is_touching(bila):
+        game.add_score(1)
+        basic.show_number(game.score())
+    basic.pause(500)
+    ciocan1.delete()
+    ciocan2.delete()
+    ciocan1 = game.create_sprite(2, 1)
+    ciocan2 = game.create_sprite(2, 2)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_forever():
+    global bila
+    basic.pause(2000)
+    bila.delete()
+    bila = game.create_sprite(randint(0, 4), 4)
+basic.forever(on_forever)
+
+def on_forever2():
+    if input.running_time() - timp_pornire >= 15000:
+        game.game_over()
+basic.forever(on_forever2)
---
+++ main.ts
@@ -0,0 +1,39 @@
+input.onButtonPressed(Button.A, function () {
+    ciocan1.change(LedSpriteProperty.X, -1)
+    ciocan2.change(LedSpriteProperty.X, -1)
+})
+input.onButtonPressed(Button.AB, function () {
+    ciocan1.change(LedSpriteProperty.Y, 2)
+    ciocan2.change(LedSpriteProperty.Y, 2)
+    if (ciocan2.isTouching(bila)) {
+        game.addScore(1)
+        basic.showNumber(game.score())
+    }
+    basic.pause(500)
+    ciocan1.delete()
+    ciocan2.delete()
+    ciocan1 = game.createSprite(2, 1)
+    ciocan2 = game.createSprite(2, 2)
+})
+input.onButtonPressed(Button.B, function () {
+    ciocan1.change(LedSpriteProperty.X, 1)
+    ciocan2.change(LedSpriteProperty.X, 1)
+})
+let ciocan2: game.LedSprite = null
+let ciocan1: game.LedSprite = null
+let bila: game.LedSprite = null
+let score = 0
+bila = game.createSprite(randint(0, 4), 4)
+ciocan1 = game.createSprite(2, 1)
+ciocan2 = game.createSprite(2, 2)
+let timp_pornire = input.runningTime()
+basic.forever(function () {
+    basic.pause(2000)
+    bila.delete()
+    bila = game.createSprite(randint(0, 4), 4)
+})
+basic.forever(function () {
+    if (input.runningTime() - timp_pornire >= 15000) {
+        game.gameOver()
+    }
+})
